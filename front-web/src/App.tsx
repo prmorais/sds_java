f01ad5a1177d712f8c83c507a52e6eb5208f8bf3
Paulo Roberto <prmorais1302@gmail.com>
2020-09-20 09:38:20 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/header";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
